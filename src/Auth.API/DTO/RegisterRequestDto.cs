fffa214f04c13d9c8d517150eeb4084b3c2a27da
Gabriel <gabriel.leme.1@hotmail.com>
2022-12-01 08:40:02 -0300

    initial

---
+++ src/Auth.API/DTO/RegisterRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Auth.API.DTO;
+
+public class RegisterRequest
+{
+    [Required]
+    public string? UserName { get; set; }
+    [Required]
+    public string? Email { get; set; }
+    [Required]
+    public string? Password { get; set; }
+}
---
+++ src/Auth.API/Services/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+﻿using Auth.API.DTO;
+
+namespace Auth.API.Services;
+
+public interface IAuthenticationService
+{
+    Task<string> Register(RegisterRequest request);
+    Task<string> Login(LoginRequest request);
+}
